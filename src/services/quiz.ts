d4a9361f9cd71682e8960c7594f740fc57adb6f9
is-savitar <savitar746233@gmail.com>
2024-09-17 01:17:02 +0100

    feat: added start command to the stacks bot

---
+++ src/services/quiz.ts
@@ -0,0 +1,32 @@
+const quiz = [
+  {
+    question: "What is stx",
+    options: ["its a blockchain", "Its a lol", "Hello", "Hi"],
+    correct: 0,
+  },
+  {
+    question: "What is stx",
+    options: ["its a blockchain", "Its a lol", "Hello", "Hi"],
+    correct: 0,
+  },
+  {
+    question: "What is stx",
+    options: ["its a blockchain", "Its a lol", "Hello", "Hi"],
+    correct: 0,
+  },
+  {
+    question: "What is stx",
+    options: ["its a blockchain", "Its a lol", "Hello", "Hi"],
+    correct: 0,
+  },
+  {
+    question: "What is stx",
+    options: ["its a blockchain", "Its a lol", "Hello", "Hi"],
+    correct: 0,
+  },
+  {
+    question: "What is stx",
+    options: ["its a blockchain", "Its a lol", "Hello", "Hi"],
+    correct: 0,
+  },
+];
