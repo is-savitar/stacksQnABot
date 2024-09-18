458242eab684986c2817f6590be58e2bf8df438b
is-savitar <savitar746233@gmail.com>
2024-09-18 15:01:56 +0100

    feat: added timer to stacks bot quiz

--- src/services/quiz.ts
+++ src/services/quiz.ts
@@ -39,59 +39,59 @@ export const quiz = [
     ],
     correct: 0,
   },
-  {
-    question: "What is Clarity?",
-    options: [
-      "A smart contract language for the Ethereum network",
-      "A smart contract language used on Stacks to provide predictable smart contracts",
-      "A Bitcoin wallet provider",
-      "A consensus algorithm used by Stacks",
-    ],
-    correct: 1,
-  },
-  {
-    question: "How are Stacks miners rewarded?",
-    options: [
-      "By minting new Stacks tokens and receiving Bitcoin from PoX participants",
-      "By staking STX and receiving Ethereum",
-      "By processing Bitcoin transactions",
-      "By validating transactions on the Stacks blockchain",
-    ],
-    correct: 0,
-  },
-  {
-    question: "What is Stacking in the Stacks ecosystem?",
-    options: [
-      "Locking STX tokens to participate in governance",
-      "Locking STX tokens to earn Bitcoin as a reward",
-      "Providing liquidity for decentralized exchanges",
-      "Staking STX to receive Ethereum rewards",
-    ],
-    correct: 1,
-  },
-  {
-    question: "What major feature does Stacks bring to Bitcoin?",
-    options: [
-      "Smart contracts and decentralized apps",
-      "Layer-2 scaling for faster transactions",
-      "Privacy features for anonymous transactions",
-      "Support for Bitcoin mining pools",
-    ],
-    correct: 0,
-  },
-  {
-    question: "How often are Bitcoin rewards distributed to Stackers?",
-    options: [
-      "Every block",
-      "Every 100 blocks",
-      "Once a day",
-      "Once per Stacks cycle (about every two weeks)",
-    ],
-    correct: 3,
-  },
-  {
-    question: "Which of the following wallets support Stacks?",
-    options: ["Stacks Wallet", "Ledger", "Hiro Wallet", "All of the above"],
-    correct: 3,
-  },
+  // {
+  //   question: "What is Clarity?",
+  //   options: [
+  //     "A smart contract language for the Ethereum network",
+  //     "A smart contract language used on Stacks to provide predictable smart contracts",
+  //     "A Bitcoin wallet provider",
+  //     "A consensus algorithm used by Stacks",
+  //   ],
+  //   correct: 1,
+  // },
+  // {
+  //   question: "How are Stacks miners rewarded?",
+  //   options: [
+  //     "By minting new Stacks tokens and receiving Bitcoin from PoX participants",
+  //     "By staking STX and receiving Ethereum",
+  //     "By processing Bitcoin transactions",
+  //     "By validating transactions on the Stacks blockchain",
+  //   ],
+  //   correct: 0,
+  // },
+  // {
+  //   question: "What is Stacking in the Stacks ecosystem?",
+  //   options: [
+  //     "Locking STX tokens to participate in governance",
+  //     "Locking STX tokens to earn Bitcoin as a reward",
+  //     "Providing liquidity for decentralized exchanges",
+  //     "Staking STX to receive Ethereum rewards",
+  //   ],
+  //   correct: 1,
+  // },
+  // {
+  //   question: "What major feature does Stacks bring to Bitcoin?",
+  //   options: [
+  //     "Smart contracts and decentralized apps",
+  //     "Layer-2 scaling for faster transactions",
+  //     "Privacy features for anonymous transactions",
+  //     "Support for Bitcoin mining pools",
+  //   ],
+  //   correct: 0,
+  // },
+  // {
+  //   question: "How often are Bitcoin rewards distributed to Stackers?",
+  //   options: [
+  //     "Every block",
+  //     "Every 100 blocks",
+  //     "Once a day",
+  //     "Once per Stacks cycle (about every two weeks)",
+  //   ],
+  //   correct: 3,
+  // },
+  // {
+  //   question: "Which of the following wallets support Stacks?",
+  //   options: ["Stacks Wallet", "Ledger", "Hiro Wallet", "All of the above"],
+  //   correct: 3,
+  // },
 ];
